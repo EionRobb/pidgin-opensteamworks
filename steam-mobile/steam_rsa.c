cd5a29471d59ae2bf03836d72fe5e0fb817e8ad8
Eion Robb <eion@robbmob.com>
2016-06-19 11:39:34 +1200

    Add GPLv3+ preamble to all source files. Fixes #145

--- steam-mobile/steam_rsa.c
+++ steam-mobile/steam_rsa.c
@@ -1,4 +1,23 @@
 /*
+ *  Steam Mobile Plugin for Pidgin
+ *  Copyright (C) 2012-2016 Eion Robb
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
 https://steamcommunity.com/mobilelogin/getrsakey?username=<steamusername>
 {"success":true,"publickey_mod":"pubkeyhex","publickey_exp":"pubkeyhex","timestamp":"165685150000"}
 
